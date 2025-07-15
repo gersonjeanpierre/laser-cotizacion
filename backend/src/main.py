f6d8a0030e59f8646f6efc417c977ec121283f05
Gerson <jalukone@outlook.com>
2025-07-15 12:48:28 -0500

    Fix run local

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,20 +3,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.api.routers import api_router
 
-# from src.infrastructure.database.database import Base, engine
-
-# # Al importar el módulo 'models', su __init__.py se ejecuta, lo que a su vez importa
-# # # todos los modelos ORM necesarios, registrándolos con Base.metadata.
-# # from src.infrastructure.persistence.models.store import StoreORM 
-# # from src.infrastructure.persistence.models.type_client import TypeClientORM 
-# # from src.infrastructure.persistence.models.product_type import ProductTypeORM
-# # from src.infrastructure.persistence.models.extra_option import ExtraOptionORM
-# # from src.infrastructure.persistence.models.product import ProductORM
-# # from src.infrastructure.persistence.models.customer import CustomerORM
-# # from src.infrastructure.persistence.models.order_status import OrderStatusORM
-# # from src.infrastructure.persistence.models.order import OrderORM
-# # from src.infrastructure.persistence.models.order_detail import OrderDetailORM, OrderDetailExtraOptionORM
-
 # # Crea las tablas en la base de datos si no existen
 # # ¡ADVERTENCIA! En entornos de producción, se recomienda usar herramientas de migración como Alembic.
 # Base.metadata.create_all(bind=engine)
@@ -32,7 +18,8 @@ app = FastAPI(
 origins = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
-    "http://localhost:8080",    
+    "http://localhost:8080",
+    "http://localhost:9000",        
 ]
 
 app.add_middleware(
